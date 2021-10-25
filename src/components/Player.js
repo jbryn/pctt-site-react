407e49dc8bb845630a87400b6ebca16ada3f9b58
Jan Bryński <brynski.jan@gmail.com>
2021-10-25 19:13:47 +0200

    play/pause next and prev song icons

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,18 +1,12 @@
 import { useEffect, useRef, useState } from "react";
 import { GrPause, GrPlay } from "react-icons/gr";
 import { IoPlayOutline } from "react-icons/io5";
-import { TiMediaPauseOutline } from "react-icons/ti";
+import {
+  TiMediaPauseOutline,
+  TiMediaFastForwardOutline,
+  TiMediaRewindOutline,
+} from "react-icons/ti";
 import { IconContext } from "react-icons";
-
-const CustomIcon = ({ icon }) => {
-  return (
-    <IconContext.Provider
-      value={{ color: "white", size: "30px", style: { margin: "10px" } }}
-    >
-      <div>{icon}</div>
-    </IconContext.Provider>
-  );
-};
 
 const Player = ({ tracks }) => {
   const [trackIndex, setTrackIndex] = useState(0);
@@ -34,14 +28,35 @@ const Player = ({ tracks }) => {
     console.log("TODO go to next");
   };
 
+  const PlayPauseButton = ({ icon, playing }) => {
+    return (
+      <div
+        onClick={() => setIsPlaying(playing)}
+        aria-label={playing ? "Play" : "Pause"}
+      >
+        <IconContext.Provider
+          value={{ color: "white", size: "30px", style: { margin: "10px" } }}
+        >
+          <div>{icon}</div>
+        </IconContext.Provider>
+      </div>
+    );
+  };
+
   return (
     <div className="player">
       <div className="track-info">
-        <CustomIcon icon={<IoPlayOutline />} />
-        <CustomIcon icon={<TiMediaPauseOutline />} />
-        <img className="cover" src={cover.url} alt="track cover" />
+        <TiMediaRewindOutline size="30px" style={{ margin: "10px" }} />
+        {isPlaying ? (
+          <PlayPauseButton icon={<TiMediaPauseOutline />} playing={false} />
+        ) : (
+          <PlayPauseButton icon={<IoPlayOutline />} playing={true} />
+        )}
+        <TiMediaFastForwardOutline size="30px" style={{ margin: "10px" }} />
+
         <h2 className="title">{title}</h2>
         <h2 className="author">{author}</h2>
+        <img className="cover" src={cover.url} alt="track cover" />
       </div>
     </div>
   );
