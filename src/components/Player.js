6134d89a2ccbd9e18e523608158c12bfe6d54741
Jan Bryński <brynski.jan@gmail.com>
2021-10-26 14:01:39 +0200

    playback progress and scrubbing

--- src/components/Player.js
+++ src/components/Player.js
@@ -5,40 +5,11 @@ const Player = ({ tracks }) => {
   const [trackIndex, setTrackIndex] = useState(0);
   const [trackProgress, setTrackProgress] = useState(0);
   const [isPlaying, setIsPlaying] = useState(false);
-
   const { title, cover, author, audio } = tracks[trackIndex];
   const audioRef = useRef(new Audio(audio.url));
   const intervalRef = useRef();
   const isReady = useRef(false);
-
   const { duration } = audioRef.current;
-
-  useEffect(() => {
-    isPlaying ? audioRef.current.play() : audioRef.current.pause();
-  }, [isPlaying]);
-
-  useEffect(() => {
-    audioRef.current.pause();
-
-    audioRef.current = new Audio(audio.url);
-    setTrackProgress(audioRef.current.currentTime);
-
-    if (isReady.current) {
-      audioRef.current.play();
-      setIsPlaying(true);
-      // startTimer();
-    } else {
-      // Set the isReady ref as true for the next pass
-      isReady.current = true;
-    }
-  }, [trackIndex]);
-
-  useEffect(() => {
-    return () => {
-      audioRef.current.pause();
-      clearInterval(intervalRef.current);
-    };
-  }, []);
 
   const toPrevTrack = () => {
     trackIndex - 1 < 0
@@ -52,6 +23,64 @@ const Player = ({ tracks }) => {
       : setTrackIndex(0);
   };
 
+  const onScrub = (value) => {
+    // Clear any timers already running
+    clearInterval(intervalRef.current);
+    audioRef.current.currentTime = value;
+    setTrackProgress(audioRef.current.currentTime);
+  };
+
+  const onScrubEnd = () => {
+    // If not already playing, start
+    if (!isPlaying) {
+      setIsPlaying(true);
+    }
+    startTimer();
+  };
+
+  const startTimer = () => {
+    clearInterval(intervalRef.current);
+    intervalRef.current = setInterval(() => {
+      if (audioRef.current.ended) {
+        toNextTrack();
+      } else {
+        setTrackProgress(audioRef.current.currentTime);
+      }
+    }, [1000]);
+  };
+
+  useEffect(() => {
+    if (isPlaying) {
+      audioRef.current.play();
+      startTimer();
+    } else {
+      clearInterval(intervalRef.current);
+      audioRef.current.pause();
+    }
+  }, [isPlaying]);
+
+  useEffect(() => {
+    audioRef.current.pause();
+    audioRef.current = new Audio(audio.url);
+    setTrackProgress(audioRef.current.currentTime);
+
+    if (isReady.current) {
+      audioRef.current.play();
+      setIsPlaying(true);
+      startTimer();
+    } else {
+      // Set the isReady ref as true for the next pass
+      isReady.current = true;
+    }
+  }, [trackIndex]);
+
+  useEffect(() => {
+    return () => {
+      audioRef.current.pause();
+      clearInterval(intervalRef.current);
+    };
+  }, []);
+
   return (
     <div className="player">
       <div className="track-info">
@@ -60,6 +89,17 @@ const Player = ({ tracks }) => {
           onPlayPauseClick={setIsPlaying}
           onPrevClick={toPrevTrack}
           onNextClick={toNextTrack}
+        />
+        <input
+          type="range"
+          value={trackProgress}
+          step="1"
+          min="0"
+          max={duration ? duration : `${duration}`}
+          className="progress"
+          onChange={(e) => onScrub(e.target.value)}
+          onMouseUp={onScrubEnd}
+          onKeyUp={onScrubEnd}
         />
         <h2 className="title">{title}</h2>
         <h2 className="author">{author}</h2>
